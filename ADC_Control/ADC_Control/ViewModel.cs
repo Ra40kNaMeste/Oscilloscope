c0bb3c2c4ee33b5b5c08886ad65e12198c246ffb
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2023-07-19 21:20:46 +0300

    Промежуточный коммит

--- ADC_Control/ADC_Control/ViewModel.cs
+++ ADC_Control/ADC_Control/ViewModel.cs
@@ -26,16 +26,16 @@ namespace ADC_Control
     sealed public class ViewModel : INotifyPropertyChanged, IDisposable
     {
 
-        [DllImport("User32.dll")]
-        private static extern bool SetCursorPos(int X, int Y);
+        //[DllImport("User32.dll")]
+        //private static extern bool SetCursorPos(int X, int Y);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-        //Mouse actions
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        ////Mouse actions
+        //private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        //private const int MOUSEEVENTF_LEFTUP = 0x04;
+        //private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        //private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
 
         #region Constructor
